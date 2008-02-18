97f0bce1d86a5720cb54b4a148b7cf7f41acb1e1
Abhay Kumar <abhay+git@opensynapse.net>
2008-02-18 02:28:50 -0800

    added helper line to all specs

--- spec/lib/core_ext/time_spec.rb
+++ spec/lib/core_ext/time_spec.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.dirname(__FILE__)) + '/../../spec_helper'
+
 describe Time, :construct do
   it "gives a time object" do
     Time.construct(2006, 1, 2, 0, 0, 0).should == Time.local(2006, 1, 2, 0, 0, 0)
