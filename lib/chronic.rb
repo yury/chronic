afc1da558996e73113e2f8824f204ed96bdc6579
Abhay Kumar <abhay+git@opensynapse.net>
2008-02-18 02:31:50 -0800

    Revert "chronic spec"
    This reverts commit 9081950bb5175410f8c08783d498b3d9bda6f625.

--- lib/chronic.rb
+++ lib/chronic.rb
@@ -42,7 +42,7 @@ require 'chronic/time_zone'
 require 'numerizer/numerizer'
 
 module Chronic
-  VERSION = "0.2.3" unless defined?(Chronic::VERSION)
+  VERSION = "0.2.3"
   
   class << self
     attr_accessor :debug
